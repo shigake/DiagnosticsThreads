7826ebf480edde5b878e77a590927dc6f1267e87
Erick Higaki <shigake@gmail.com>
2020-08-16 13:58:18 -0300

    Add project files.

---
+++ Thread/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfThreads = 64;
+            ThreadRun tws = new ThreadRun();
+            for (int i = 0; i < numberOfThreads; i++)
+                new Thread(new ThreadStart(tws.NewThread)).Start();
+
+        }
+    }
+}
---
+++ Thread/ThreadRun.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ThreadProgram
+{
+    public class ThreadRun
+    {
+        private int place;
+
+        public void NewThread()
+        {
+            var sW = new Stopwatch();
+            sW.Start();
+            Hanoi();
+            sW.Stop();
+            place++;
+            Console.WriteLine($"time: {sW.Elapsed} thread: {place}");
+        }
+
+        private static void Hanoi()
+        {
+            char startPeg = 'A'; // start tower in output
+            char endPeg = 'C'; // end tower in output
+            char tempPeg = 'B'; // temporary tower in output
+            int totalDisks = 30; // number of disks
+
+            SolveTowers(totalDisks, startPeg, endPeg, tempPeg);
+        }
+
+        private static void SolveTowers(int n, char startPeg, char endPeg, char tempPeg)
+        {
+            if (n > 0)
+            {
+                SolveTowers(n - 1, startPeg, tempPeg, endPeg);
+                //Console.WriteLine("Move disk from " + startPeg + ' ' + endPeg);
+                SolveTowers(n - 1, tempPeg, endPeg, startPeg);
+            }
+        }
+
+
+    }
+}
